7a1ee9c674105139f73ca2ba073c9bf2837d953b
sweetkang <xuyangkang@gmail.com>
2012-07-17 19:54:08 +0800

    c++ temp

--- cpptemp.cpp
+++ cpptemp.cpp
@@ -1,5 +1,13 @@
 #include <cstdio>
 
+#ifndef __SWEETHEADER
+#define __SWEETHEADER
+
+#include <iostream>
+#define debug(x) do std::cout << #x << " = " << x << "\n"
+
+#endif
+
 int main() {
     return 0;
 }
