29d7944b06710a08f4af1ab01b71fa405717153f
sweetkang <xuyangkang@gmail.com>
2012-07-17 20:17:17 +0800

    Java IO updated

--- javatemp.java
+++ javatemp.java
@@ -1,11 +1,12 @@
 import java.util.*;
+import java.io.*;
 
 class Main {
 
     void run() {
         //Scanner in = new Scanner(System.in);
         MyReader in = new MyReader();
-
+        String str;
     }
 
     public static void main(String args[]) {
@@ -21,13 +22,21 @@ class MyReader {
     BufferedReader br = new BufferedReader (
             new InputStreamReader (System.in));
     StringTokenizer in;
-    String next() throws Exception {
-        while (in == null || !in.hasMoreTokens()) {
-            in = new StringTokenizer(br.readLine());
+    String next() {
+        try {
+            while (in == null || !in.hasMoreTokens()) {
+                // Read a new line and split it into tokens
+                in = new StringTokenizer(br.readLine());
+            }
+            // return next token
+            return in.nextToken();
+        } catch (Exception e) {
+            // EOF
+            return null;
         }
-        return in.nextToken();
     }
-    int nextInt() throws Exception {
+    // Transform the tokens into other types
+    int nextInt() {
         return Integer.parseInt(next());
     }
 }
